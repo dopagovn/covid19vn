e60b6ed3b91333727c20b395831dd21766082c4c
Nguyen Quoc Thinh <thinhnguyen@Nguyens-MacBook-Pro.local>
2021-05-08 16:15:07 +0700

    Updates

---
+++ precache-manifest.efd6fc9793a5dfe9dbbbc336b751331c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2832e9329b316ea3bd350c1d0cf542",
+    "url": "/covid19vn/index.html"
+  },
+  {
+    "revision": "78d1bbce6d86aa8b7e5d",
+    "url": "/covid19vn/static/css/main.ef34580b.chunk.css"
+  },
+  {
+    "revision": "70a6b8aad239118aec94",
+    "url": "/covid19vn/static/js/2.4bfdcf3e.chunk.js"
+  },
+  {
+    "revision": "5e5c5aaefd726bfc2ad51722d14c5d57",
+    "url": "/covid19vn/static/js/2.4bfdcf3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78d1bbce6d86aa8b7e5d",
+    "url": "/covid19vn/static/js/main.6ce5e4f2.chunk.js"
+  },
+  {
+    "revision": "48da7874f0699dbc4050",
+    "url": "/covid19vn/static/js/runtime-main.36f89a83.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19vn/static/media/image.d7265326.png"
+  }
+]);
